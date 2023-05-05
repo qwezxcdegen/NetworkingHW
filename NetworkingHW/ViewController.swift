b6e755cb58c15a85a27634a665188ce132537554
qwezxcdegen <ifomstep@icloud.com>
2023-05-06 01:16:03 +0500

    Initial Commit

---
+++ NetworkingHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingHW
+//
+//  Created by Степан Фоминцев on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
